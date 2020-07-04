fb001d257080608588b386e265149033e1f1fd94
Gabriel <gabriielpagote@gmail.com>
2020-07-03 22:15:54 -0300

    Fix "controller"

--- controller/SistemasController.php
+++ controller/SistemasController.php
@@ -1,16 +1,14 @@
 <?php
 
 session_start();
-
 require "./repository/SistemasRepositoryPDO.php";
 require "./model/Sistema.php";
 
 class SistemasController{
-    public function index(){
     
-    $sistemasRepository = new SistemasRepositoryPDO();
-    return $sistemasRepository->listarTodos();
-        
+    public function index(){
+        $sistemasRepository = new SistemasRepositoryPDO();
+        return $sistemasRepository->listarTodos();
     }
 
     public function save($request){
@@ -23,18 +21,20 @@ class SistemasController{
         if(gettype($upload)=="string"){
             $sistema->poster = $upload;
         }
-
-        if ($sistemasRepository->salvar($sistema))
-            $_SESSION["msg"] = "Sistema cadastrado com sucesso";
-        else
-            echo "\n falha ao inserir sistemas\n";
-
+        
+        if ($sistemasRepository->salvar($sistema)) 
+                $_SESSION["msg"] = "Sistema cadastrado com sucesso";
+        else 
+                $_SESSION["msg"] = "Falha ao cadastrar sistema";
+        
         header("Location: /");
+        
     }
 
     private function savePoster($file){
         $posterDir = "imagens/posters/";
-    $posterPath = $posterDir . basename($file["poster_file"]["name"]);
+        $posterPath = $posterDir . basename($file["poster_file"]["name"]);
         $posterTmp = $file["poster_file"]["tmp_name"];
-        move_uploaded_file($posterTmp, $posterPath);
+
     }
+}
